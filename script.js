dad35ea1d3ae5a209ca1fe055e0dc8f37b42aae4
Keerthi Surisetty <keerthisurisetty@Keerthis-MacBook-Air.local>
2025-09-13 14:47:16 -0400

    fixed project section to have only two subheadings

--- script.js
+++ script.js
@@ -9,27 +9,23 @@ menuToggle.addEventListener('click', () => {
 function setupCarousel(carouselId) {
   const container = document.getElementById(carouselId);
   const track = container.querySelector('.carousel-track');
+  const cards = track.children;
   const leftArrow = container.querySelector('.carousel-arrow.left');
   const rightArrow = container.querySelector('.carousel-arrow.right');
-  const cards = track.children;
 
-  const visibleCards = Math.floor(container.offsetWidth / cards[0].offsetWidth); // number of cards visible
   const cardWidth = cards[0].offsetWidth + parseInt(getComputedStyle(track).gap);
-
   let position = 0;
 
-  // Move carousel
   function moveRight() {
-    position -= cardWidth; 
-    const maxPosition = -(cardWidth * (cards.length - visibleCards));
+    position -= cardWidth;
+    const maxPosition = -(cardWidth * (cards.length - 4)); // show 4 cards at a time
     if (position < maxPosition) position = 0;
     track.style.transform = `translateX(${position}px)`;
   }
 
   function moveLeft() {
     position += cardWidth;
-    const maxPosition = 0;
-    if (position > maxPosition) position = -(cardWidth * (cards.length - visibleCards));
+    if (position > 0) position = -(cardWidth * (cards.length - 4));
     track.style.transform = `translateX(${position}px)`;
   }
 
